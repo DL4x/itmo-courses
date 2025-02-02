254820ce7dd7f936bda3cfb37a6dcd4dfc890af2
Shulpin Egor <117765106+DL4x@users.noreply.github.com>
2025-02-02 22:50:54 +0300

    Delete data-analysis directory

--- data-analysis/2-knn-DL4x/scripts/config.py
+++
@@ -1,6 +0,0 @@
-# Category (target class)
-TARGET_CLASS = 'Бренд'
-# CSV file from previous lab
-DATASET_FILE = 'dataset/superstep_sneakers.csv'
-# Unnecessary data from CSV file
-UNNECESSARY_COLUMNS = ['Ссылка на изображение']
